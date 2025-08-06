89aa23fba8b515d12298c4a52f88cb89eae6c254
Yolisses <iamurise@gmail.com>
2025-08-06 11:10:16 -0300

    Create SleepPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:go_sleep/sleep_page.dart';
 import 'package:go_sleep/systemd_page.dart';
 
 void main(List<String> args) {
-  print('args');
-  print(args);
   final shouldSleep = args.contains('--sleep');
-  print(shouldSleep);
-  runApp(const MyApp());
+  runApp(MyApp(shouldSleep: shouldSleep));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key, required this.shouldSleep});
+
+  final bool shouldSleep;
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        brightness: Brightness.dark,
-      ),
-      home: SystemdPage(),
+      theme: ThemeData(brightness: Brightness.dark),
+      home: shouldSleep ? SleepPage() : SystemdPage(),
     );
   }
 }
---
+++ lib/sleep_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SleepPage extends StatelessWidget {
+  const SleepPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Text('Go sleep, buddy'));
+  }
+}
