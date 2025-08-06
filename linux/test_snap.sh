e0f4c65eeb7277d4a87b3bff51908c688a0c3c82
Yolisses <iamurise@gmail.com>
2025-08-06 13:28:04 -0300

    Lint test_snap.sh

--- linux/test_snap.sh
+++ linux/test_snap.sh
@@ -1,1 +1,4 @@
-snapcraft && sudo snap install --dangerous --devmode ./go-sleep_0.1_amd64.snap && go-sleep;
+snapcraft && \
+sudo snap remove go-sleep && \
+sudo snap install --dangerous --devmode ./go-sleep_0.1_amd64.snap && \
+go-sleep;
