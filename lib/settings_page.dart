49f915ba9f13aecb783b832137ba191d7b50073e
Yolisses <iamurise@gmail.com>
2025-08-06 09:41:43 -0300

    Add time picker in settings page

--- lib/settings_page.dart
+++ lib/settings_page.dart
@@ -4,31 +4,59 @@ class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
 
   @override
-  State<StatefulWidget> createState() => _SettingsPageState();
+  State<SettingsPage> createState() => _SettingsPageState();
 }
 
 class _SettingsPageState extends State<SettingsPage> {
-  bool _enabled = false;
+  final _formKey = GlobalKey<FormState>();
 
-  void _onEnabledChange(bool value) {
-    setState(() {
-      _enabled = value;
-    });
+  TimeOfDay? _selectedTime = TimeOfDay(hour: 22, minute: 0);
+  bool _isEnabled = false;
+
+  Future<void> _selectTime(BuildContext context) async {
+    final TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.now(),
+    );
+    if (picked != null && picked != _selectedTime) {
+      setState(() {
+        _selectedTime = picked;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Row(
+      appBar: AppBar(title: Text('Time Form')),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
             children: [
-              Text('Enabled'),
-              Switch(value: _enabled, onChanged: _onEnabledChange),
+              SwitchListTile(
+                title: const Text('Enabled'),
+                value: _isEnabled,
+                onChanged: (bool value) {
+                  setState(() {
+                    _isEnabled = value;
+                  });
+                },
+              ),
+              TextFormField(
+                readOnly: true,
+                decoration: InputDecoration(labelText: 'Time of the day'),
+                onTap: () => _selectTime(context),
+                validator: (value) =>
+                    _selectedTime == null ? 'Please select a time' : null,
+                controller: TextEditingController(
+                  text: _selectedTime?.format(context) ?? '',
+                ),
+              ),
             ],
           ),
-        ],
+        ),
       ),
     );
   }
