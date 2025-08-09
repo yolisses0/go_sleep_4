615faa6e90544bb4c40fb1cfe471ad2c98d74810
Yolisses <iamurise@gmail.com>
2025-08-09 17:25:19 -0300

    Fix Windows window title

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -26,7 +26,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"go_sleep", origin, size)) {
+  if (!window.Create(L"Go Sleep", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
