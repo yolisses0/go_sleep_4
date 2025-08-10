9b0ca86a7b3913e9413faaac8a83a7d2a01209cc
Yolisses <iamurise@gmail.com>
2025-08-10 17:47:48 -0300

    Install workmanager

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,30 @@
-import 'dart:isolate';
+import 'dart:developer';
 
-import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 import 'package:flutter/material.dart';
-import 'package:go_sleep/custom_overlay.dart';
-import 'package:system_alert_window/system_alert_window.dart';
-
-@pragma("vm:entry-point")
-void overlayMain() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: CustomOverlay()));
-}
-
-void _openOverlayWindow() async {
-  await SystemAlertWindow.sendMessageToOverlay('show system window');
-  SystemAlertWindow.showSystemWindow(
-    height: 200,
-    width: 200,
-    gravity: SystemWindowGravity.CENTER,
-    prefMode: SystemWindowPrefMode.OVERLAY,
-    layoutParamFlags: [SystemWindowFlags.FLAG_NOT_FOCUSABLE],
-  );
-}
+import 'package:workmanager/workmanager.dart';
 
 @pragma('vm:entry-point')
-void launchApp() async {
-  final DateTime now = DateTime.now();
-  final int isolateId = Isolate.current.hashCode;
-  print("[$now] Hello, world! isolate=$isolateId");
-  // This will bring the app to the foreground
-  _openOverlayWindow();
+void callbackDispatcher() {
+  Workmanager().executeTask((task, inputData) async {
+    log("Background task: $task");
+    // Your background work here
+    return Future.value(true);
+  });
+}
+
+void _schedule() async {
+  log("Schedule");
+  Workmanager().registerOneOffTask(
+    "task-id",
+    "simpleTask",
+    initialDelay: Duration(seconds: 10),
+  );
 }
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  await AndroidAlarmManager.initialize();
+  Workmanager().initialize(callbackDispatcher);
   runApp(const MyApp());
-
-  final int alarmId = 0;
-  await AndroidAlarmManager.periodic(
-    const Duration(seconds: 10),
-    alarmId,
-    launchApp,
-  );
-}
-
-Future<void> _requestPermissions() async {
-  await SystemAlertWindow.requestPermissions(
-    prefMode: SystemWindowPrefMode.OVERLAY,
-  );
-}
-
-void _closeOverlayWindow() async {
-  SystemAlertWindow.closeSystemWindow(prefMode: SystemWindowPrefMode.OVERLAY);
 }
 
 class MyApp extends StatelessWidget {
@@ -65,18 +37,7 @@ class MyApp extends StatelessWidget {
         body: Center(
           child: ListView(
             children: [
-              TextButton(
-                onPressed: _requestPermissions,
-                child: Text('Request permission'),
-              ),
-              TextButton(
-                onPressed: _openOverlayWindow,
-                child: Text('Open overlay'),
-              ),
-              TextButton(
-                onPressed: _closeOverlayWindow,
-                child: Text('Close overlay'),
-              ),
+              TextButton(onPressed: _schedule, child: Text('Schedule')),
             ],
           ),
         ),
